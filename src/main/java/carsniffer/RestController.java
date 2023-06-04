f25c8fb34faac2de16664efe0301248c668d39a1
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-04 13:08:47 +0200

    Create RestController.java

---
+++ src/main/java/carsniffer/RestController.java
@@ -0,0 +1,22 @@
+package carsniffer;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class RestController {
+
+	@Autowired
+	private Server server;
+
+	@PostMapping("/tutorials")
+	public ResponseEntity<Void> getAllTutorials(@RequestParam byte[] input) {
+		try {
+			
+
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+  
+}
