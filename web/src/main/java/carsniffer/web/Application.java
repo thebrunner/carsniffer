67677ef2e5a043d09f1c5cd10765c4f65f5bac7c
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-04 17:10:02 +0200

    Multi maven project

--- web/src/main/java/carsniffer/web/Application.java
+++ web/src/main/java/carsniffer/web/Application.java
@@ -1,4 +1,4 @@
-package carsniffer;
+package carsniffer.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
--- web/src/main/java/carsniffer/web/RestController.java
+++ web/src/main/java/carsniffer/web/RestController.java
@@ -1,4 +1,4 @@
-package carsniffer;
+package carsniffer.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
