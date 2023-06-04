c707efc744684635117b889bfd57e6f6c1bd04e9
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-04 18:16:59 +0200

    Testing

--- web/src/main/java/carsniffer/web/RESTController.java
+++ web/src/main/java/carsniffer/web/RESTController.java
@@ -1,6 +1,9 @@
 package carsniffer.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +12,25 @@ import carsniffer.server.Server;
 @RestController
 public class RESTController {
 
+	private final static Logger LOGGER = LoggerFactory.getLogger(RESTController.class);
+
 	@Autowired
 	private Server server;
 
 	@PostMapping("/receive")
-	public String receive() {
-		server.receive(new byte[] { 10 });
-		return "ok";
+	public ResponseEntity<Void> receive(byte[] byteInput) {
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Receiving: " + byteInput);
+		}
+
+		server.receive(byteInput);
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Successfully converting " + byteInput);
+		}
+
+		return ResponseEntity.ok(null);
 	}
 
 }
