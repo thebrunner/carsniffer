7e0be719d8c8009bd4bb892a2105bf4530be2e77
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-04 12:38:25 +0200

    Update Server.java

--- SRC/Main/Java/carsniffer/Server.java
+++ SRC/Main/Java/carsniffer/Server.java
@@ -6,8 +6,13 @@ public class Server {
   @Autowire
   private InputConverter inputConverter;
   
+  @Autowire
+  private Storage storage;
+  
   public void receive(byte[] arrayInput) {
     final var input = inputConverter.convert(arrayInput);
+    
+    storage.store(input);
   }
   
 }
