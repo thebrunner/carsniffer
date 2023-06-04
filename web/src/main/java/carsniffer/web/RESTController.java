263f7d8bc215e7efa3af5b3c31547fbd1a4d96fe
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-04 18:08:49 +0200

    Swagger-UI

--- web/src/main/java/carsniffer/web/RESTController.java
+++ web/src/main/java/carsniffer/web/RESTController.java
@@ -1,7 +1,6 @@
 package carsniffer.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +11,11 @@ public class RESTController {
 
 	@Autowired
 	private Server server;
-	
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
 
 	@PostMapping("/receive")
 	public String receive() {
-		server.receive(new byte[] {10});
+		server.receive(new byte[] { 10 });
 		return "ok";
 	}
-	
+
 }
