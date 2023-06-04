29ee79103f8bcae4462e74c674cb4893dabc66cf
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-04 14:27:33 +0200

    Adding tests

---
+++ src/test/java/carsniffer/ServerIT.java
@@ -0,0 +1,18 @@
+package carsniffer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerIT {
+
+	@Test
+	void test() {
+		
+	}
+
+}
---
+++ src/test/java/carsniffer/ServerTest.java
@@ -0,0 +1,36 @@
+package carsniffer;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ServerTest {
+
+	@Mock
+	InputConverter inputConverter;
+	
+	@Mock
+	Storage storage;
+	
+	@InjectMocks
+	Server server;
+	
+	@Test
+	void testReceive() {
+		byte[] b = new byte[] {1};
+		
+		var input = new Input();
+		when(inputConverter.convert(b)).thenReturn(input);
+		
+		server.receive(b);
+	
+		verify(storage).store(input);
+	}
+
+}
