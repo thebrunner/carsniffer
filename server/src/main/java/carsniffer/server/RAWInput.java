a213758a9b042af0b8755712ba9caa66944870cf
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-05 20:05:36 +0200

    arrival LocalDateTime

--- server/src/main/java/carsniffer/server/RAWInput.java
+++ server/src/main/java/carsniffer/server/RAWInput.java
@@ -2,5 +2,10 @@ package carsniffer.server;
 
 import java.util.BitSet;
 
-public record RAWInput(BitSet raw, int length) {
+public record RAWInput(BitSet raw, int length, LocalDateTime arrival) {
+  
+  public RAWInput of(BitSet raw, int length) {
+    return new RAWInput(raw, length, LocalDateTime.now());
+  }
+    
 }
