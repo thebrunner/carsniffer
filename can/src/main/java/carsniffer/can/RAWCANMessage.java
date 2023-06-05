2db3ad0c226fcb1607e322c21d49b4668ac26833
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-05 21:27:04 +0200

    Update RAWCANMessage.java

--- can/src/main/java/carsniffer/can/RAWCANMessage.java
+++ can/src/main/java/carsniffer/can/RAWCANMessage.java
@@ -2,6 +2,8 @@ package carsniffer.can;
 
 import carsniffer.server.RAWInput;
 
-public record RAWCANMessage(RAWInput identifier, RAWInput data, RAWInput crc, RAWInput ack) {
-
+public record RAWCANMessage(RAWInput identifier, RAWInput extendedIdentifier, RAWInput data, RAWInput crc, RAWInput ack) {
+  public static RAWCANMessage ofBaseFrame(RAWInput identifier, RAWInput data, RAWInput crc, RAWInput ack) {
+    return new RAWCANMessage(identifier, new BitSet(), data, crc, ack);
+  }
 }
