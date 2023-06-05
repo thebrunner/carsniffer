19241257f68c66f362aa43c531012de68a474403
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-05 21:27:38 +0200

    Update RAWInput.java

--- server/src/main/java/carsniffer/server/RAWInput.java
+++ server/src/main/java/carsniffer/server/RAWInput.java
@@ -4,7 +4,7 @@ import java.util.BitSet;
 
 public record RAWInput(BitSet raw, int length, LocalDateTime arrival) {
   
-  public RAWInput of(BitSet raw, int length) {
+  public static RAWInput of(BitSet raw, int length) {
     return new RAWInput(raw, length, LocalDateTime.now());
   }
     
