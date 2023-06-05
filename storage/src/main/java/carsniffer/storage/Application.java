c47ce9519f8df47783f56a484a31af75de4920d3
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-05 16:55:50 +0200

    Create Application.java

---
+++ storage/src/main/java/carsniffer/storage/Application.java
@@ -0,0 +1,9 @@
+package carsniffer.storage;
+
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+    public static void main(final String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
