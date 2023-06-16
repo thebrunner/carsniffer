204370e0f9cb6ff222669fa5e208ab319e4b144c
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-06-16 16:11:45 +0200

    Update Server.java

--- server/src/main/java/carsniffer/server/Server.java
+++ server/src/main/java/carsniffer/server/Server.java
@@ -4,18 +4,16 @@ public class Server {
 
 	private InputConverter inputConverter;
 
-	private Storage storage;
+	private Collection<Storage> storage;
 
-	public Server(InputConverter inputConverter, Storage storage) {
+	public Server(InputConverter inputConverter, Collecction<Storage> storage) {
 		this.inputConverter = inputConverter;
 		this.storage = storage;
 	}
 
 	public void receive(RAWInput rawInput) throws CarSnifferException {
-		
 		final var input = inputConverter.convert(rawInput);
-		
-		storage.store(input);
+		storage.foreach(s -> s.store(input));
 	}
 
 }
