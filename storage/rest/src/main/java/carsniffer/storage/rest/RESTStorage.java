e2dcb66971087246a9b445d907be8d9aa101fb18
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-09-10 12:01:29 +0200

    Update RESTStorage.java

--- storage/rest/src/main/java/carsniffer/storage/rest/RESTStorage.java
+++ storage/rest/src/main/java/carsniffer/storage/rest/RESTStorage.java
@@ -32,7 +32,7 @@ public class RESTStorage implements Storage {
 				.body(BodyInserters.fromValue(input)) //
 				.retrieve() //
 				.onStatus(HttpStatusCode::isError, 
-					  error -> Mono.error(new CarSnifferException("Cannot send: " + error)) //
+					  error -> Mono.error(new CarSnifferException("Cannot send: " + error))) //
 				.toBodilessEntity() //
 				.log() //
 		;
