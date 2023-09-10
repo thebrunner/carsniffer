3961064cebb42895663d9feeba57832eab9181bd
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-09-10 10:47:50 +0200

    Update Server.java

--- server/src/main/java/carsniffer/server/Server.java
+++ server/src/main/java/carsniffer/server/Server.java
@@ -29,7 +29,7 @@ public class Server {
 					}
 				});
 			} catch (CarSnifferException e) {
-				LOGGER.error("Error converting with " + s.getClass().getSimpleName(), e);
+				LOGGER.error("Error converting", e);
 			}
 		});
 	}
