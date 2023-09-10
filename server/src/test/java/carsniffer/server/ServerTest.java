7956bf72354ef86d310c773c27ebeca6758747c4
thebrunner <christian.thebrunner.brunner@googlemail.com>
2023-09-10 10:54:03 +0200

    Update ServerTest.java

--- server/src/test/java/carsniffer/server/ServerTest.java
+++ server/src/test/java/carsniffer/server/ServerTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.BitSet;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
